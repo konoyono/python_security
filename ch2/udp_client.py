2a04e773641b0d95d456840515f042d5f918fd1c
岡田俊樹 <>
2018-11-11 18:46:34 +0900

    add tcp_server code

--- ch2/udp_client.py
+++ ch2/udp_client.py
@@ -1,7 +1,7 @@
 import socket
 
 target_host = "127.0.0.1"
-target_port = 80
+target_port = 9999
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -9,6 +9,6 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 client.sendto(b"ABCDEFG", (target_host, target_port))
 
-data, addr = client.recv(4096)
+data, addr = client.recvfrom(4096)
 
 print(data)
