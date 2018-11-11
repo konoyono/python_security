21395baf710102b656851fc3ea939d879f7d1937
岡田俊樹 <>
2018-11-11 18:23:21 +0900

    add udp_client code

---
+++ ch2/udp_client.py
@@ -0,0 +1,14 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# client.connect((target_host, target_port))
+
+client.sendto(b"ABCDEFG", (target_host, target_port))
+
+data, addr = client.recv(4096)
+
+print(data)
